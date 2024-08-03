18a90df2146c086290beb5c615e0cdd9c2ea4ccf
rithvikr <rithvik.redrouthu@gmail.com>
2024-08-03 11:13:36 -0600

    Add install handler

--- src/commands/command_handler.rs
+++ src/commands/command_handler.rs
@@ -1,6 +1,8 @@
 use async_trait::async_trait;
 use std::env::Args;
 
+use super::install::InstallHandler;
+
 #[async_trait]
 pub trait CommandHandler {
     fn parse(&mut self, args: &mut Args) -> ();
@@ -17,5 +19,10 @@ pub async fn handle_args(mut args: Args) -> () {
             println!("No help menu implemented yet.");
             return;
         }
+    };
+
+    let mut command_handler: Box<dyn CommandHandler> = match command.to_lowercase().as_str() {
+        "install" => Box::<InstallHandler>::default(),
+        _ => return
     };
 }
---
+++ src/commands/install.rs
@@ -0,0 +1,10 @@
+use super::command_handler::CommandHandler;
+
+#[derive(Debug, Default)]
+pub struct InstallHandler {
+    package_name: String,
+}
+
+impl InstallHandler {
+    // TODO: Write install handler logic
+}
