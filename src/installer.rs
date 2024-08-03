11d00eb7f326e12735b0ab9b1d9290999ab62676
rithvikr <rithvik.redrouthu@gmail.com>
2024-08-03 16:19:30 -0600

    Parse author

--- src/installer.rs
+++ src/installer.rs
@@ -2,16 +2,24 @@ use std::env::Args;
 
 #[derive(Debug, Default)]
 pub struct Installer {
+    author: String,
     package_name: String,
 }
 
 impl Installer {
-    fn parse_package_details(package_details: String) -> String {
-        let mut split = package_details.split("@");
+    fn parse_package_details(package_details: String) -> (String, String) {
+        let mut split = package_details.split("/");
+
+        let author = split
+            .next()
+            .expect("Provided package author is empty")
+            .to_string();
+
         let name = split
             .next()
             .expect("Provided package name is empty")
             .to_string();
-        name
+
+        (author, name)
     }
 }
