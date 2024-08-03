9acf97df317507d66c0911febead91d9b6331e7a
rithvikr <rithvik.redrouthu@gmail.com>
2024-08-03 10:58:41 -0600

    Command not found

--- src/commands/command_handler.rs
+++ src/commands/command_handler.rs
@@ -6,3 +6,16 @@ pub trait CommandHandler {
     fn parse(&mut self, args: &mut Args) -> ();
     async fn execute(&self) -> ();
 }
+
+pub async fn handle_args(mut args: Args) -> () {
+    args.next();
+
+    let command = match args.next() {
+        Some(command) => command,
+        None => {
+            // TODO: Implement help menu
+            println!("No help menu implemented yet.");
+            return;
+        }
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+mod commands;
+
 use std::env;
+
+use commands::command_handler;
 
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let parse_result = command_handler::handle_args(env::args()).await;
+    if let Err(err) = parse_result {
+        println!("Command does not exist, Error: {}", err);
+    }
 }
