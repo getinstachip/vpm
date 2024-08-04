f065b838e4241cf3481bf748ff9fdc3331cc742c
sathvikr <sathvik.redrouthu@gmail.com>
2024-08-04 01:46:09 -0700

    toml

--- src/embedding.rs
+++ src/embedding.rs
@@ -12,9 +12,7 @@ use walkdir::WalkDir;
 use std::env;
 
 pub const ES_URL: &str = "https://69bc680d7967407080cd9090e3c12a25.us-central1.gcp.cloud.es.io:443";
-// pub const ES_API_KEY: &str = "TEFSYkFwRUJjd1F5SmpPNG5JUXc6el83R2NQU3NUTEc5OVRlMnltb2g1QQ==";
 pub const ES_API_KEY: &str = "UWdUV0M1RUJjd1F5SmpPNHRJVlU6Ui1tenFqaUFReFc5d0k2ODJSVnBldw==";
-// pub const ES_API_KEY: &str = "essu_YTIxNU9VTTFSVUphWVZwM1NscEhWbEZQYld3NmIzZExhVmhDUlc5VVZDMXZUVGxIVUVkR1JrVXpRUT09AAAAALJJ010=";
 
 pub(crate) fn create_client() -> Result<Elasticsearch, Box<dyn std::error::Error>> {
     let conn_pool = SingleNodeConnectionPool::new(ES_URL.parse()?);
