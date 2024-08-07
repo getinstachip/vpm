4ee9134b2696c6dbcc89a4eaf57b6d24b0a2dba0
sathvikr <sathvik.redrouthu@gmail.com>
2024-08-07 10:44:28 -0700

    updated install script

--- install_mac_bin.sh
+++ install_mac_bin.sh
@@ -15,12 +15,17 @@ fi
 
 # Download the binary
 echo "Downloading VPM v${VPM_VERSION}..."
-curl -L -o vpm "https://github.com/getinstachip/vpm/releases/download/v${VPM_VERSION}/vpm-x86_64-apple-darwin"
+curl -LO vpm "https://github.com/getinstachip/vpm/releases/download/v${VPM_VERSION}/vpm-x86_64-apple-darwin"
 
 # Make the binary executable
 chmod +x vpm
 
 # Move the binary to a directory in PATH
 sudo mv vpm /usr/local/bin/
+
+# Clean up
+cd ..
+rm -rf "vpm-${VPM_VERSION}"
+rm "v${VPM_VERSION}.tar.gz"
 
 echo "VPM v${VPM_VERSION} has been installed successfully!"
