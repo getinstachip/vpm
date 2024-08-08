ba3251e02d9692f0143ee5ba737fcfc7626dcc14
rithvikr <rithvik.redrouthu@gmail.com>
2024-08-08 16:04:47 -0600

    Add --url install

--- src/cmd/install.rs
+++ src/cmd/install.rs
@@ -1,15 +1,34 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
+use std::process::Command;
 
 use crate::cmd::{Execute, Install};
 
 impl Execute for Install {
     fn execute(&self) -> Result<()> {
+        const LOCATION: &str = "./vpm_modules";
+
         if let Some(url) = &self.url {
-            // TODO: Add url install logic
+            install_from_url(url, LOCATION)?;
         } else if let Some(name) = &self.package_name {
             // TODO: Add package install logic
         }
 
         Ok(())
     }
+}
+
+fn install_from_url(url: &String, location: &str) -> Result<()>{
+    Command::new("git")
+        .arg("clone")
+        .arg("--depth")
+        .arg("1")
+        .arg("--single-branch")
+        .arg("--jobs")
+        .arg("4")
+        .arg(url)
+        .arg(location)
+        .status()
+        .with_context(|| format!("Failed to clone repository from URL: '{}'", url))?;
+    
+    Ok(())
 }
