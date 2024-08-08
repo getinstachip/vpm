921c711ee11c751f616317c18696feefec344c27
rithvikr <rithvik.redrouthu@gmail.com>
2024-08-08 11:45:13 -0600

    Add config and mod to main

---
+++ src/config.rs
@@ -0,0 +1,19 @@
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct Config {
+    /// The configured `Workspace` root path
+    pub workspace_root: PathBuf,
+    /// The current working directory
+    pub cwd: PathBuf,
+    /// VPM's home directory
+    pub home: Option<PathBuf>,
+}
+
+impl Config  {
+    // TODO: Create workspace
+    // #[must_use]
+    // pub fn workspace(&self) -> Workspace {
+    //     Workspace::new(&self.workspace_root, self)
+    // }
+}
