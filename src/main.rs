a9e66494d516d01fa4f4479e4ea94d1de407ded2
rithvikr <rithvik.redrouthu@gmail.com>
2024-08-08 11:16:36 -0600

    Remove backtrace

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,17 @@ mod updater;
 mod command_handler;
 mod includer;
 
+use std::env;
+
 use clap::Parser;
 use command_handler::Args;
 
 #[tokio::main]
 async fn main() {
+    // Forcibly disable backtraces
+    env::remove_var("RUST_LIB_BACKTRACE");
+    env::remove_var("RUST_BACKTRACE");
+
     let result = command_handler::handle_args(Args::parse()).await;
     if let Err(e) = result {
         eprintln!("Failed to handle arguments. Error: {}", e);
