0d06bfb897b96caa10972d394608cccb7171b6dd
rithvikr <rithvik.redrouthu@gmail.com>
2024-08-15 18:50:15 -0600

    Couple speed optimizations

--- src/cmd/install.rs
+++ src/cmd/install.rs
@@ -1,7 +1,8 @@
 use anyhow::{Context, Result};
+use once_cell::sync::Lazy;
 use regex::Regex;
 use std::collections::HashSet;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::{fs, process::Command};
 use tree_sitter::{Node, Parser, Query, QueryCursor};
 use walkdir::WalkDir;
@@ -10,73 +11,67 @@ use crate::cmd::{Execute, Install};
 
 const STD_LIB_URL: &str = "https://github.com/getinstachip/openchips";
 
+static URL_REGEX: Lazy<Regex> = Lazy::new(|| {
+    Regex::new(r"^(https?://|git://|ftp://|file://|www\.)[\w\-\.]+\.\w+(/[\w\-\.]*)*/?$").unwrap()
+});
+
 impl Execute for Install {
     fn execute(&self) -> Result<()> {
-        if !PathBuf::from("./vpm_modules").exists() {
-            fs::create_dir("./vpm_modules")?;
-        }
-        if let (Some(url), Some(name)) = (&self.url, &self.package_name) {
-            println!("Installing module '{}' from URL: '{}'", name, url);
-            install_module_from_url(name, url)?;
-        } else if let Some(arg) = &self.url.as_ref().or(self.package_name.as_ref()) {
-            if Regex::new(r"^(https?://|git://|ftp://|file://|www\.)[\w\-\.]+\.\w+(/[\w\-\.]*)*/?$")
-                .unwrap()
-                .is_match(arg)
-            {
-                let url = arg.to_string();
-                println!("Installing repository from URL: '{}'", url);
-                install_repo_from_url(&url, "./vpm_modules/")?;
-            } else {
-                let name = arg.to_string();
-                println!("Installing module '{}' from standard library", name);
-                install_module_from_url(&name, STD_LIB_URL)?;
-            }
-        } else {
-            println!("Command not found!");
-        }
+        fs::create_dir_all("./vpm_modules")?;
 
-        Ok(())
+        match (&self.url, &self.package_name) {
+            (Some(url), Some(name)) => {
+                println!("Installing module '{}' from URL: '{}'", name, url);
+                install_module_from_url(name, url)
+            }
+            (Some(url), None) | (None, Some(url)) if URL_REGEX.is_match(url) => {
+                println!("Installing repository from URL: '{}'", url);
+                install_repo_from_url(url, "./vpm_modules/")
+            }
+            (None, Some(name)) => {
+                println!("Installing module '{}' from standard library", name);
+                install_module_from_url(name, STD_LIB_URL)
+            }
+            _ => {
+                println!("Command not found!");
+                Ok(())
+            }
+        }
     }
 }
 
-fn name_from_url(url: &str) -> Result<String> {
-    Ok(url
-        .rsplit('/')
-        .find(|segment| !segment.is_empty())
-        .unwrap_or_default()
-        .to_string())
+fn name_from_url(url: &str) -> &str {
+    url.rsplit('/').find(|&s| !s.is_empty()).unwrap_or_default()
 }
 
 pub fn install_module_from_url(module: &str, url: &str) -> Result<()> {
-    let package_name = name_from_url(url)?.to_string();
+    let package_name = name_from_url(url);
+    let tmp_path = PathBuf::from("/tmp").join(package_name);
 
     install_repo_from_url(url, "/tmp/")?;
 
     let mut visited = HashSet::new();
-    process_module(&package_name, module, &mut visited)?;
+    process_module(package_name, module, &mut visited)?;
 
-    fs::remove_dir_all(format!("/tmp/{}", package_name))?;
+    fs::remove_dir_all(tmp_path)?;
 
     Ok(())
 }
 
 fn process_module(package_name: &str, module: &str, visited: &mut HashSet<String>) -> Result<()> {
-    dbg!(&module);
-    visited.insert(module.replace(".v", ""));
-    for entry in WalkDir::new(format!("/tmp/{}", package_name)) {
-        let entry = entry?;
-        if Some(module) == entry.file_name().to_str() {
-            let path = entry.path();
-            let contents = fs::read_to_string(path)?;
+    let module_name = module.strip_suffix(".v").unwrap_or(module);
+    if !visited.insert(module_name.to_string()) {
+        return Ok(());
+    }
+    println!("Processing module '{}'", module_name);
+    let tmp_path = PathBuf::from("/tmp").join(package_name);
+    for entry in WalkDir::new(&tmp_path).into_iter().filter_map(Result::ok) {
+        if entry.file_name() == module {
+            let contents = fs::read_to_string(entry.path())?;
             let mut parser = Parser::new();
-            parser
-                .set_language(tree_sitter_verilog::language())
-                .expect("Error loading Verilog grammar");
+            parser.set_language(tree_sitter_verilog::language())?;
 
-            let tree = parser
-                .parse(&contents, None)
-                .expect("Failed to parse the file");
-
+            let tree = parser.parse(&contents, None).context("Failed to parse file")?;
             let root_node = tree.root_node();
 
             let header_content = generate_headers(root_node, &contents)?;
@@ -85,9 +80,9 @@ fn process_module(package_name: &str, module: &str, visited: &mut HashSet<String
                 header_content,
             )?;
 
-            for submodule in get_submodules(root_node, &contents, visited)? {
+            for submodule in get_submodules(root_node, &contents)? {
                 if !visited.contains(&submodule) {
-                    process_module(package_name, &format!("{}.v", &submodule), visited)?;
+                    process_module(package_name, &format!("{}.v", submodule), visited)?;
                 }
             }
 
@@ -99,29 +94,31 @@ fn process_module(package_name: &str, module: &str, visited: &mut HashSet<String
 }
 
 fn generate_headers(root_node: Node, contents: &str) -> Result<String> {
-    let query = Query::new(
-        tree_sitter_verilog::language(),
-        "(module_declaration
-            (module_header
-                (module_keyword)
-                (simple_identifier) @module_name)
-            (module_nonansi_header
-                (parameter_port_list)? @params
-                (list_of_ports) @ports)
+    static QUERY: Lazy<Query> = Lazy::new(|| {
+        Query::new(
+            tree_sitter_verilog::language(),
+            "(module_declaration
+                (module_header
+                    (module_keyword)
+                    (simple_identifier) @module_name)
+                (module_nonansi_header
+                    (parameter_port_list)? @params
+                    (list_of_ports) @ports)
+            )
+            (module_declaration
+                (module_header
+                    (module_keyword)
+                    (simple_identifier) @module_name)
+                (module_ansi_header
+                    (parameter_port_list)? @params
+                    (list_of_port_declarations)? @ports)
+            )",
         )
-        (module_declaration
-            (module_header
-                (module_keyword)
-                (simple_identifier) @module_name)
-            (module_ansi_header
-                (parameter_port_list)? @params
-                (list_of_port_declarations)? @ports)
-        )",
-    )
-    .expect("Failed to create query");
+        .expect("Failed to create query")
+    });
 
     let mut query_cursor = QueryCursor::new();
-    let matches = query_cursor.matches(&query, root_node, contents.as_bytes());
+    let matches = query_cursor.matches(&QUERY, root_node, contents.as_bytes());
 
     let mut header_content = String::new();
 
@@ -139,57 +136,48 @@ fn generate_headers(root_node: Node, contents: &str) -> Result<String> {
                 _ => {}
             }
         }
-        header_content.push_str(&format!("module {} ", module_name));
-
-        if !params.is_empty() {
-            header_content.push_str("#(\n");
-            header_content.push_str(params);
-            header_content.push_str(") ");
-        }
-
-        header_content.push_str("(\n");
-        header_content.push_str(ports);
-        header_content.push_str(");\n\n");
-
-        header_content.push_str("// TODO: Add module implementation\n\n");
-        header_content.push_str(&format!("endmodule // {}\n\n", module_name));
+        
+        header_content.push_str(&format!(
+            "module {} {}(\n{}\n{});\n\n// TODO: Add module implementation\n\nendmodule // {}\n\n",
+            module_name,
+            if params.is_empty() { "" } else { "#(\n" },
+            params,
+            ports,
+            module_name
+        ));
     }
 
     Ok(header_content)
 }
 
-fn get_submodules(
-    root_node: Node,
-    contents: &str,
-    visited: &mut HashSet<String>,
-) -> Result<Vec<String>> {
-    let query = Query::new(
-        tree_sitter_verilog::language(),
-        "(module_or_generate_item 
-            (module_instantiation 
-                (simple_identifier) @module_submodule
+fn get_submodules(root_node: Node, contents: &str) -> Result<HashSet<String>> {
+    static QUERY: Lazy<Query> = Lazy::new(|| {
+        Query::new(
+            tree_sitter_verilog::language(),
+            "(module_or_generate_item 
+                (module_instantiation 
+                    (simple_identifier) @module_submodule
+                )
             )
+            (module_or_generate_item 
+                (udp_instantiation 
+                    (simple_identifier) @module_submodule
+                )
+            )",
         )
-        (module_or_generate_item 
-            (udp_instantiation 
-                (simple_identifier) @module_submodule
-            )
-        )",
-    )
-    .expect("Failed to create query");
+        .expect("Failed to create query")
+    });
 
     let mut query_cursor = QueryCursor::new();
-    let matches = query_cursor.matches(&query, root_node, contents.as_bytes());
+    let matches = query_cursor.matches(&QUERY, root_node, contents.as_bytes());
 
-    let mut submodules = Vec::new();
+    let mut submodules = HashSet::new();
 
     for match_ in matches {
         for capture in match_.captures {
             if capture.index == 0 {
                 let capture_text = &contents[capture.node.byte_range()];
-                if !visited.contains(capture_text) {
-                    submodules.push(capture_text.to_string());
-                }
+                submodules.insert(capture_text.to_string());
             }
         }
     }
@@ -198,27 +186,21 @@ fn get_submodules(
 }
 
 fn install_repo_from_url(url: &str, location: &str) -> Result<()> {
-    let repo_path = PathBuf::from(location).join(name_from_url(url)?);
+    let repo_path = Path::new(location).join(name_from_url(url));
 
-    fn clone_repo(url: &str, repo_path: &str) -> Result<()> {
-        Command::new("git")
-            .args([
-                "clone",
-                "--depth",
-                "1",
-                "--single-branch",
-                "--jobs",
-                "4",
-                url,
-                repo_path,
-            ])
-            .status()
-            .with_context(|| format!("Failed to clone repository from URL: '{}'", url))?;
-
-        Ok(())
-    }
-
-    clone_repo(url, repo_path.to_str().unwrap_or_default())?;
+    Command::new("git")
+        .args([
+            "clone",
+            "--depth",
+            "1",
+            "--single-branch",
+            "--jobs",
+            "4",
+            url,
+            repo_path.to_str().unwrap_or_default(),
+        ])
+        .status()
+        .with_context(|| format!("Failed to clone repository from URL: '{}'", url))?;
 
     Ok(())
 }
