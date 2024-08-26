ef4b1500df49cdfc259dcb55ec601ad3f45de165
Jag <jag.maddipatla@gmail.com>
2024-08-26 01:19:48 -0400

    iVerilog sim capability

--- obj_dir/verilated_timing.d
+++
@@ -1,8 +0,0 @@
-verilated_timing.o: \
-  /usr/local/Cellar/verilator/5.028/share/verilator/include/verilated_timing.cpp \
-  /usr/local/Cellar/verilator/5.028/share/verilator/include/verilated_timing.h \
-  /usr/local/Cellar/verilator/5.028/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/5.028/share/verilator/include/verilatedos.h \
-  /usr/local/Cellar/verilator/5.028/share/verilator/include/verilated_config.h \
-  /usr/local/Cellar/verilator/5.028/share/verilator/include/verilated_types.h \
-  /usr/local/Cellar/verilator/5.028/share/verilator/include/verilated_funcs.h
