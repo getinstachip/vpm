69625e933bcbdf1e76938ea16aa654108878cd85
Rithvik Redrouthu <63871606+rithvikru@users.noreply.github.com>
2024-09-02 21:14:45 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -70,7 +70,7 @@ main() {
 # Parse the arguments passed and set variables accordingly.
 parse_args() {
     # BIN_DIR_DEFAULT="${HOME}/.local/bin"
-    BIN_DIR_DEFAULT=/usr/local/bin
+    BIN_DIR_DEFAULT=/usr/bin
     # MAN_DIR_DEFAULT="${HOME}/.local/share/man"
     SUDO_DEFAULT="sudo"
 
