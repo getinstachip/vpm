2c436bba3e6665187e22ff5711ea4340da40ca75
PVadde <pvadde@purdue.edu>
2024-09-09 14:09:13 -0700

    remove temp dir if exists

--- src/cmd/include.rs
+++ src/cmd/include.rs
@@ -702,6 +702,9 @@ pub fn include_repo_from_url(url: &str, location: &str) -> Result<()> {
 }
 
 pub fn clone_repo(url: &str, repo_path: &Path) -> Result<()> {
+    if repo_path.exists() {
+        fs::remove_dir_all(repo_path)?;
+    }
     Command::new("git")
         .args([ "clone", "--depth", "1", "--single-branch", "--jobs", "4",
             url, repo_path.to_str().unwrap_or_default(),
