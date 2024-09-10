6a538e5d23e6650453412bd0c1442d6eb5383499
PVadde <pvadde@purdue.edu>
2024-09-10 13:40:49 -0700

    modify upgrade function

--- src/cmd/upgrade.rs
+++ src/cmd/upgrade.rs
@@ -15,13 +15,23 @@ impl Execute for Upgrade {
 }
 
 fn upgrade_vpm() -> Result<()> {
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg("curl -sSfL https://raw.githubusercontent.com/getinstachip/vpm-internal/main/install.sh | sh")
-        .output()?;
+    if cfg!(unix) {
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg("curl -sSfL https://raw.githubusercontent.com/getinstachip/vpm-internal/main/install.sh | sh")
+            .output()?;
 
-    if !output.status.success() {
-        return Err(anyhow::anyhow!("Upgrade command failed"));
+        if !output.status.success() {
+            return Err(anyhow::anyhow!("Upgrade command failed"));
+        }
+    } else if cfg!(windows) {
+        println!("To upgrade VPM on Windows, please follow these steps:");
+        println!("1. Visit https://github.com/getinstachip/vpm/releases/latest");
+        println!("2. Download the appropriate .exe file for your system");
+        println!("3. Run the downloaded .exe file to complete the upgrade");
+        return Ok(());
+    } else {
+        return Err(anyhow::anyhow!("Unsupported operating system"));
     }
 
     Ok(())
